d280d47306fee6f8a77458bdcc69c652142a4c05
rashmi0710 <dongrerashmi15@gmail.com>
2025-09-10 23:05:59 +0530

    corected css name

--- frontend/green-house-gas-emission/src/components/Navbar.jsx
+++ frontend/green-house-gas-emission/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "./navbar.css";
+import "./Navbar.css";
 
 // Font Awesome imports
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
